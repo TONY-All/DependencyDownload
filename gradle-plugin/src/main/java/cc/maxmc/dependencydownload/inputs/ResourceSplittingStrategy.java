20c60f5f973ce01e58aa9bb247eff4489e9f28a6
TONY_All <admin@minicraft.top>
2023-01-06 13:36:16 +0800

    remove unused codes

--- gradle-plugin/src/main/java/cc/maxmc/dependencydownload/inputs/ResourceSplittingStrategy.java
+++
@@ -1,9 +0,0 @@
-package cc.maxmc.dependencydownload.inputs;
-
-public enum ResourceSplittingStrategy {
-
-    SINGLE_FILE,
-    TOP_LEVEL_DEPENDENCIES,
-    ALL_DEPENDENCIES
-
-}
--- jarinjar/loader/src/main/java/cc/maxmc/dependencydownload/jarinjar/loader/exception/LoadingException.java
+++
@@ -1,8 +0,0 @@
-package cc.maxmc.dependencydownload.jarinjar.loader.exception;
-
-public class LoadingException extends RuntimeException {
-
-    public LoadingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
